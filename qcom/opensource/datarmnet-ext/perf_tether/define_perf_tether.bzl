LineageOS/android_kernel_qcom_sm8750-modules

build: bazel: Track kernel and modules path change
Change-Id: Ibbc4db8d67ecc8842b7051c73f3b56f9739be8ad

qcom/opensource/datarmnet-ext/perf_tether/define_perf_tether.bzl
@@ -3,7 +3,7 @@ load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 
 def define_perf_tether(target, variant):
     kernel_build_variant = "{}_{}".format(target, variant)
-    include_base = "../../../{}".format(native.package_name())
+    include_base = "../../../../../{}".format(native.package_name())
 
     ddk_module(
         name = "{}_perf_tether".format(kernel_build_variant),
@@ -11,11 +11,11 @@ def define_perf_tether(target, variant):
         srcs = [
             "rmnet_perf_tether_main.c",
         ],
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
-            "//vendor/qcom/opensource/datarmnet:rmnet_core_headers",
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
+            "//vendor/qcom/sm8750-modules/qcom/opensource/datarmnet:rmnet_core_headers",
         ],
         copts = ["-Wno-misleading-indentation"],
     )

qcom/opensource/datarmnet-ext/wlan/define_wlan.bzl
@@ -3,7 +3,7 @@ load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 
 def define_wlan(target, variant):
     kernel_build_variant = "{}_{}".format(target, variant)
-    include_base = "../../../{}".format(native.package_name())
+    include_base = "../../../../../{}".format(native.package_name())
 
     ddk_module(
         name = "{}_wlan".format(kernel_build_variant),
@@ -20,11 +20,11 @@ def define_wlan(target, variant):
             "rmnet_wlan_stats.c",
             "rmnet_wlan_stats.h",
         ],
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
-            "//vendor/qcom/opensource/datarmnet:rmnet_core_headers",
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
+            "//vendor/qcom/sm8750-modules/qcom/opensource/datarmnet:rmnet_core_headers",
         ],
         copts = ["-Wno-misleading-indentation"],
     )

qcom/opensource/datarmnet/define_modules.bzl
@@ -3,7 +3,7 @@ load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 
 def define_modules(target, variant):
     kernel_build_variant = "{}_{}".format(target, variant)
-    include_base = "../../../{}".format(native.package_name())
+    include_base = "../../../../../{}".format(native.package_name())
 
     #The below will take care of the defconfig
     #include_defconfig = ":{}_defconfig".format(variant)
@@ -44,11 +44,11 @@ def define_modules(target, variant):
                 ],
             },
         },
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
-            "//vendor/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/dataipa:include_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/dataipa:include_headers",
         ],
     )
 
@@ -76,15 +76,15 @@ def define_modules(target, variant):
         local_defines = [
             "RMNET_TRACE_INCLUDE_PATH={}/core".format(include_base),
         ],
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
             ":rmnet_core_headers",
             ":{}_rmnet_ctl".format(kernel_build_variant),
-            "//vendor/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
-            "//vendor/qcom/opensource/datarmnet-ext/mem:{}_rmnet_mem".format(kernel_build_variant),
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/dataipa:include_headers",
-            "//vendor/qcom/opensource/datarmnet-ext/mem:rmnet_mem_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
+            "//vendor/qcom/sm8750-modules/qcom/opensource/datarmnet-ext/mem:{}_rmnet_mem".format(kernel_build_variant),
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/dataipa:include_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/datarmnet-ext/mem:rmnet_mem_headers",
         ],
     )
 

qcom/opensource/graphics-kernel/build/kgsl_defs.bzl
@@ -1,6 +1,6 @@
 load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 load("//build/bazel_common_rules/dist:dist.bzl", "copy_to_dist_dir")
-load("//msm-kernel:target_variants.bzl", "get_all_la_variants")
+load("//vendor/qcom/kernel:target_variants.bzl", "get_all_la_variants")
 
 msm_kgsl_includes = [
     "include/linux/msm_kgsl.h",
@@ -92,23 +92,23 @@ def external_deps(target, variant):
     # Add msm_hw_fence in the dependency and defconfig lists for targets that use it
     if target in [ "pineapple" ]:
         deplist = deplist + [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:{}_msm_hw_fence".format(tv),
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers/hw_fence:{}_msm_hw_fence".format(tv),
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
             ]
         defconfigs = defconfigs + [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:defconfig"
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers/hw_fence:defconfig"
             ]
 
     # Add synx-kernel in the dependency list for targets that use it for hardware fences
     if target in [ "sun", "niobe" ]:
         deplist = deplist + [
-            "//vendor/qcom/opensource/synx-kernel:{}_modules".format(tv),
-            "//vendor/qcom/opensource/synx-kernel:synx_headers"
+            "//vendor/qcom/sm8750-modules/qcom/opensource/synx-kernel:{}_modules".format(tv),
+            "//vendor/qcom/sm8750-modules/qcom/opensource/synx-kernel:synx_headers"
             ]
 
     if target in [ "monaco", "parrot" ]:
         deplist = deplist + [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
             ]
 
     native.genrule(
@@ -123,7 +123,7 @@ def external_deps(target, variant):
 def define_target_variant_module(target, variant):
     tv = "{}_{}".format(target, variant)
     rule_name = "{}_msm_kgsl".format(tv)
-    kernel_build = "//msm-kernel:{}".format(tv)
+    kernel_build = "//vendor/qcom/kernel:{}".format(tv)
 
     ext_deps = external_deps(target, variant)
 
@@ -149,7 +149,7 @@ def define_target_variant_module(target, variant):
             "CONFIG_DEVFREQ_GOV_QCOM_ADRENO_TZ": { False: [ "governor_msm_adreno_tz.c" ] },
             "CONFIG_DEVFREQ_GOV_QCOM_GPUBW_MON": { False: [ "governor_gpubw_mon.c" ] }
         },
-        deps = [ "//msm-kernel:all_headers" ] + ext_deps,
+        deps = [ "//vendor/qcom/kernel:all_headers" ] + ext_deps,
         includes = ["include", "."],
         kernel_build = kernel_build,
         visibility = ["//visibility:private"]

qcom/opensource/mm-drivers/msm_ext_display/define_msm_ext_display.bzl
@@ -1,6 +1,6 @@
 load("//build/kernel/kleaf:kernel.bzl", "ddk_module", "ddk_submodule")
 load("//build/bazel_common_rules/dist:dist.bzl", "copy_to_dist_dir")
-load("//msm-kernel:target_variants.bzl", "get_all_variants")
+load("//vendor/qcom/kernel:target_variants.bzl", "get_all_variants")
 
 def _define_module(target, variant):
     tv = "{}_{}".format(target, variant)
@@ -10,9 +10,9 @@ def _define_module(target, variant):
         out = "msm_ext_display.ko",
         defconfig = "defconfig",
         kconfig = "Kconfig",
-        deps = ["//msm-kernel:all_headers",
-                "//vendor/qcom/opensource/mm-drivers:mm_drivers_headers"],
-        kernel_build = "//msm-kernel:{}".format(tv),
+        deps = ["//vendor/qcom/kernel:all_headers",
+                "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers:mm_drivers_headers"],
+        kernel_build = "//vendor/qcom/kernel:{}".format(tv),
     )
 
     copy_to_dist_dir(

qcom/opensource/mm-drivers/sync_fence/define_sync_fence.bzl
@@ -1,6 +1,6 @@
 load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 load("//build/bazel_common_rules/dist:dist.bzl", "copy_to_dist_dir")
-load("//msm-kernel:target_variants.bzl", "get_all_variants")
+load("//vendor/qcom/kernel:target_variants.bzl", "get_all_variants")
 
 def _define_module(target, variant):
     tv = "{}_{}".format(target, variant)
@@ -11,10 +11,10 @@ def _define_module(target, variant):
         kconfig = "Kconfig",
         defconfig = "defconfig",
         deps = [
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/mm-drivers:mm_drivers_headers",
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers:mm_drivers_headers",
         ],
-        kernel_build = "//msm-kernel:{}".format(tv),
+        kernel_build = "//vendor/qcom/kernel:{}".format(tv),
     )
 
     copy_to_dist_dir(

qcom/opensource/synx-kernel/synx_modules.bzl
@@ -20,8 +20,8 @@ register_synx_module(
     # Configs are handled by config_options = []
     config_deps = {
         "CONFIG_QTI_HW_FENCE" : [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:%b_msm_hw_fence",
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers/hw_fence:%b_msm_hw_fence",
+            "//vendor/qcom/sm8750-modules/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers",
         ],
     },
 )

elginsk8r/android_kernel_qcom_sm8450-modules

qcom/opensource/datarmnet-ext/perf_tether/define_perf_tether.bzl
@@ -3,7 +3,7 @@ load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 
 def define_perf_tether(target, variant):
     kernel_build_variant = "{}_{}".format(target, variant)
-    include_base = "../../../{}".format(native.package_name())
+    include_base = "../../../../../{}".format(native.package_name())
 
     ddk_module(
         name = "{}_perf_tether".format(kernel_build_variant),
@@ -11,11 +11,11 @@ def define_perf_tether(target, variant):
         srcs = [
             "rmnet_perf_tether_main.c",
         ],
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
-            "//vendor/qcom/opensource/datarmnet:rmnet_core_headers",
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
+            "//vendor/qcom/sm8450-modules/qcom/opensource/datarmnet:rmnet_core_headers",
         ],
         copts = ["-Wno-misleading-indentation"],
     )

qcom/opensource/datarmnet-ext/wlan/define_wlan.bzl
@@ -3,7 +3,7 @@ load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 
 def define_wlan(target, variant):
     kernel_build_variant = "{}_{}".format(target, variant)
-    include_base = "../../../{}".format(native.package_name())
+    include_base = "../../../../../{}".format(native.package_name())
 
     ddk_module(
         name = "{}_wlan".format(kernel_build_variant),
@@ -20,11 +20,11 @@ def define_wlan(target, variant):
             "rmnet_wlan_stats.c",
             "rmnet_wlan_stats.h",
         ],
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
-            "//vendor/qcom/opensource/datarmnet:rmnet_core_headers",
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/datarmnet:{}_rmnet_core".format(kernel_build_variant),
+            "//vendor/qcom/sm8450-modules/qcom/opensource/datarmnet:rmnet_core_headers",
         ],
         copts = ["-Wno-misleading-indentation"],
     )

qcom/opensource/datarmnet/define_modules.bzl
@@ -3,7 +3,7 @@ load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 
 def define_modules(target, variant):
     kernel_build_variant = "{}_{}".format(target, variant)
-    include_base = "../../../{}".format(native.package_name())
+    include_base = "../../../../../{}".format(native.package_name())
 
     #The below will take care of the defconfig
     #include_defconfig = ":{}_defconfig".format(variant)
@@ -44,11 +44,11 @@ def define_modules(target, variant):
                 ],
             },
         },
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
-            "//vendor/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/dataipa:include_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/dataipa:include_headers",
         ],
     )
 
@@ -76,15 +76,15 @@ def define_modules(target, variant):
         local_defines = [
             "RMNET_TRACE_INCLUDE_PATH={}/core".format(include_base),
         ],
-        kernel_build = "//msm-kernel:{}".format(kernel_build_variant),
+        kernel_build = "//vendor/qcom/kernel:{}".format(kernel_build_variant),
         deps = [
             ":rmnet_core_headers",
             ":{}_rmnet_ctl".format(kernel_build_variant),
-            "//vendor/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
-            "//vendor/qcom/opensource/datarmnet-ext/mem:{}_rmnet_mem".format(kernel_build_variant),
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/dataipa:include_headers",
-            "//vendor/qcom/opensource/datarmnet-ext/mem:rmnet_mem_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/dataipa:{}_ipam".format(kernel_build_variant),
+            "//vendor/qcom/sm8450-modules/qcom/opensource/datarmnet-ext/mem:{}_rmnet_mem".format(kernel_build_variant),
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/dataipa:include_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/datarmnet-ext/mem:rmnet_mem_headers",
         ],
     )
 

qcom/opensource/graphics-kernel/build/kgsl_defs.bzl
@@ -1,6 +1,6 @@
 load("//build/kernel/kleaf:kernel.bzl", "ddk_module", "ddk_headers")
 load("//build/bazel_common_rules/dist:dist.bzl", "copy_to_dist_dir")
-load("//msm-kernel:target_variants.bzl", "get_all_la_variants")
+load("//vendor/qcom/kernel:target_variants.bzl", "get_all_la_variants")
 
 msm_kgsl_includes = [
     "include/linux/msm_kgsl.h",
@@ -92,23 +92,23 @@ def external_deps(target, variant):
     # Add msm_hw_fence in the dependency and defconfig lists for targets that use it
     if target in [ "pineapple" ]:
         deplist = deplist + [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:{}_msm_hw_fence".format(tv),
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers/hw_fence:{}_msm_hw_fence".format(tv),
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
             ]
         defconfigs = defconfigs + [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:defconfig"
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers/hw_fence:defconfig"
             ]
 
     # Add synx-kernel in the dependency list for targets that use it for hardware fences
     if target in [ "sun", "niobe" ]:
         deplist = deplist + [
-            "//vendor/qcom/opensource/synx-kernel:{}_modules".format(tv),
-            "//vendor/qcom/opensource/synx-kernel:synx_headers"
+            "//vendor/qcom/sm8450-modules/qcom/opensource/synx-kernel:{}_modules".format(tv),
+            "//vendor/qcom/sm8450-modules/qcom/opensource/synx-kernel:synx_headers"
             ]
 
     if target in [ "monaco", "parrot" ]:
         deplist = deplist + [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers"
             ]
 
     native.genrule(
@@ -123,7 +123,7 @@ def external_deps(target, variant):
 def define_target_variant_module(target, variant):
     tv = "{}_{}".format(target, variant)
     rule_name = "{}_msm_kgsl".format(tv)
-    kernel_build = "//msm-kernel:{}".format(tv)
+    kernel_build = "//vendor/qcom/kernel:{}".format(tv)
 
     ext_deps = external_deps(target, variant)
 
@@ -149,7 +149,7 @@ def define_target_variant_module(target, variant):
             "CONFIG_DEVFREQ_GOV_QCOM_ADRENO_TZ": { False: [ "governor_msm_adreno_tz.c" ] },
             "CONFIG_DEVFREQ_GOV_QCOM_GPUBW_MON": { False: [ "governor_gpubw_mon.c" ] }
         },
-        deps = [ "//msm-kernel:all_headers" ] + ext_deps,
+        deps = [ "//vendor/qcom/kernel:all_headers" ] + ext_deps,
         includes = ["include", "."],
         kernel_build = kernel_build,
         visibility = ["//visibility:private"]

qcom/opensource/mm-drivers/msm_ext_display/define_msm_ext_display.bzl
@@ -1,6 +1,6 @@
 load("//build/kernel/kleaf:kernel.bzl", "ddk_module", "ddk_submodule")
 load("//build/bazel_common_rules/dist:dist.bzl", "copy_to_dist_dir")
-load("//msm-kernel:target_variants.bzl", "get_all_variants")
+load("//vendor/qcom/kernel:target_variants.bzl", "get_all_variants")
 
 def _define_module(target, variant):
     tv = "{}_{}".format(target, variant)
@@ -10,9 +10,9 @@ def _define_module(target, variant):
         out = "msm_ext_display.ko",
         defconfig = "defconfig",
         kconfig = "Kconfig",
-        deps = ["//msm-kernel:all_headers",
-                "//vendor/qcom/opensource/mm-drivers:mm_drivers_headers"],
-        kernel_build = "//msm-kernel:{}".format(tv),
+        deps = ["//vendor/qcom/kernel:all_headers",
+                "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers:mm_drivers_headers"],
+        kernel_build = "//vendor/qcom/kernel:{}".format(tv),
     )
 
     copy_to_dist_dir(

qcom/opensource/mm-drivers/sync_fence/define_sync_fence.bzl
@@ -1,6 +1,6 @@
 load("//build/kernel/kleaf:kernel.bzl", "ddk_module")
 load("//build/bazel_common_rules/dist:dist.bzl", "copy_to_dist_dir")
-load("//msm-kernel:target_variants.bzl", "get_all_variants")
+load("//vendor/qcom/kernel:target_variants.bzl", "get_all_variants")
 
 def _define_module(target, variant):
     tv = "{}_{}".format(target, variant)
@@ -11,10 +11,10 @@ def _define_module(target, variant):
         kconfig = "Kconfig",
         defconfig = "defconfig",
         deps = [
-            "//msm-kernel:all_headers",
-            "//vendor/qcom/opensource/mm-drivers:mm_drivers_headers",
+            "//vendor/qcom/kernel:all_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers:mm_drivers_headers",
         ],
-        kernel_build = "//msm-kernel:{}".format(tv),
+        kernel_build = "//vendor/qcom/kernel:{}".format(tv),
     )
 
     copy_to_dist_dir(

qcom/opensource/synx-kernel/synx_modules.bzl
@@ -20,8 +20,8 @@ register_synx_module(
     # Configs are handled by config_options = []
     config_deps = {
         "CONFIG_QTI_HW_FENCE" : [
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:%b_msm_hw_fence",
-            "//vendor/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers/hw_fence:%b_msm_hw_fence",
+            "//vendor/qcom/sm8450-modules/qcom/opensource/mm-drivers/hw_fence:hw_fence_headers",
         ],
     },
 )